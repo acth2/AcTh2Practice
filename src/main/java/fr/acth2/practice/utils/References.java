5c88dc5ebf6f8d79d10d0d43247bc43388b22a93
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-12-31 22:00:31 +0100

    P6: [+] Fix de bug ou les joueurs déconnectés sont toujours dans la fil et ajout d'un logger
    [+] Fix
    [+] Logger
    [-] La bousole est envelé pour pas que le joueur ai puisse queue pendant un fight

--- src/main/java/fr/acth2/practice/utils/References.java
+++ src/main/java/fr/acth2/practice/utils/References.java
@@ -3,5 +3,5 @@ package fr.acth2.practice.utils;
 public class References {
     public static final String MODID = "acth2practice";
     public static final String NAME = "AcTh2Practice";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 }
