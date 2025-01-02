79fd766d0fca2eb9cc81e65ca5945d0cf2c6c771
AcTh2 <68511655+acth2@users.noreply.github.com>
2025-01-02 21:32:45 +0100

    P22: AJOUT MAJEUR: [+] Systeme de queue multiples et kits différents !
    [+] Il y a désormai des queues et des kits différents tel que
    Nodebuff et Diamond ! de nombreux autres bug ont été fix aussi !

--- src/main/java/fr/acth2/practice/utils/References.java
+++ src/main/java/fr/acth2/practice/utils/References.java
@@ -3,5 +3,5 @@ package fr.acth2.practice.utils;
 public class References {
     public static final String MODID = "acth2practice";
     public static final String NAME = "AcTh2Practice";
-    public static final String VERSION = "3.2.5";
+    public static final String VERSION = "4.2.5";
 }
