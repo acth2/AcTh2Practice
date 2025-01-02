40014476e5428164fb6930458d995832f0931271
AcTh2 <68511655+acth2@users.noreply.github.com>
2025-01-03 00:50:42 +0100

    P23: [+] Ajout du kit nodebuff + ajout des potions
    [+] PotionFiller
    [+] Potion*
    [+] NoDebuff kit

---
+++ src/main/java/fr/acth2/practice/misc/PotionFiller.java
@@ -0,0 +1,43 @@
+package fr.acth2.practice.misc;
+
+import fr.acth2.practice.PracticeMod;
+import net.minecraft.server.level.ServerPlayer;
+import org.apache.logging.log4j.core.jmx.Server;
+
+public class PotionFiller {
+
+    // THAT CODE BELOW IS GOING TO BE STINKY I WILL CHANGE IT SOON I PROMISE BRUH
+    public static void fill(ServerPlayer player) {
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+        PracticeMod.givePotions(player, "strong_healing", 1);
+    }
+}
