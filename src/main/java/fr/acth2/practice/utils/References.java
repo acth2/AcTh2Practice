8eef90efb72164d08f1ab653b8ecfaed4d693afc
AcTh2 <68511655+acth2@users.noreply.github.com>
2025-01-04 00:34:26 +0100

    P28: [+] Aide appliqué au développeurs en méttants les valeurs importantes dans la classe réferences
    [-] PracticeMod.SPAWN_POS;
    [-] PracticeMod.BANNED_WORDS;
    [-] Les arenes
    
    [+] References.SPAWN_POS;
    [+] References.BANNED_WORDS;
    [+] Les arenes

--- src/main/java/fr/acth2/practice/utils/References.java
+++ src/main/java/fr/acth2/practice/utils/References.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class References {
     public static final String MODID = "acth2practice";
-    public static final String NAME = "AcTh2Practice";
+    public static final String NAME = "AcTh2Practice"; 
     public static final String VERSION = "4.3.5";
 
     //  CHANGEZ PAR VOS VALEURS ICI:
